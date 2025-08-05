f168029abefa681f622b36098d4351b2311dd23f
HENRIQUE ALAN RODRIGUES MARQUES <henrique.armarques@senacsp.edu.br>
2025-08-05 19:58:40 -0300

    build: instalando bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { useState } from 'react'
 import './App.css'
 
@@ -8,6 +9,8 @@ function App() {
     <>
     <h1 className='display-3'>Calculadora Dólar</h1>
     <img className='logo' src="/public/dollar.png" alt="" />
+    <br />
+    <button className='btn btn-primary'>Click Aqui</button>
     </>
   )
 }
